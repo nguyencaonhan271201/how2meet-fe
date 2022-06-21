f403e3850d069ad253044a0cf707e8a12cbc2a2a
Nguyen Cao Nhan <nguyencaonhan2712@gmail.com>
2022-06-22 01:30:09 +0700

    Remove console logs in upload adapter

--- src/containers/MeetingMinute/UploadAdapter.js
+++ src/containers/MeetingMinute/UploadAdapter.js
@@ -22,13 +22,10 @@ export class UploadAdapter {
             function (snapshot) {
               var progress =
                 (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-              console.log("Upload is " + progress + "% done");
               switch (snapshot.state) {
                 case "paused":
-                  console.log("Upload is paused");
                   break;
                 case "running":
-                  console.log("Upload is running");
                   break;
               }
             },
@@ -43,15 +40,12 @@ export class UploadAdapter {
                   break;
 
                 case "storage/unknown":
-                  reject(
-                    "Unknown error occurred, inspect error.serverResponse"
-                  );
+                  reject("Unknown error occurred, inspect error.serverResponse");
                   break;
               }
             },
             function () {
               getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
-                console.log("File available at", downloadURL);
                 resolve({
                   default: downloadURL
                 });
